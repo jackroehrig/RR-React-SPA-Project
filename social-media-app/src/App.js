9402d4f3d6f9bbba25be31630e82a775edd13e06
jackroehrig <jack.roe13@gmail.com>
2022-01-31 19:24:31 -0600

    Starting project code

---
+++ social-media-app/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+
+/* 
+    
+      Navbar w/ logo & name
+      
+      App
+        Navbar(Logo to home route, my profile route, search profiles/tinks?, new tink)
+        Homescreen Route
+          Recent Tinks
+          Profiles to Follow
+        User Profile Route
+          Profile Info
+          What I'm Tinking
+          Tinks
+        ???
+        Searching Profiles/Tinks
+          SearchBar
+          ResultsArea
+
+    */
+
+function App() {
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
